105045e97dc825f0b8a62d95973b8feabb565209
Andrey Kurdyumov <kant2002@gmail.com>
2015-10-19 18:56:47 +0600

    Update setting application rewritten to be able dynamically change parameters

--- tools/updateSettings.js
+++ tools/updateSettings.js
@@ -16,13 +16,38 @@ client.on("error", function (err) {
   client.close();
 });
 
+var deviceIpMask = "255.255.255.0";
+var data = [
+{
+	ip: "192.168.1.218",
+	gateway: "192.168.1.108",
+	serial: "144-1-31-95"
+},
+{
+	ip: "192.168.1.217",
+	gateway: "192.168.1.108",
+	serial: "199-1-112-40"
+}
+];
+
 client.on("listening", function () {
 	var address = client.address();
   	console.log("Server listening " + address.address + ":" + address.port);
 	console.log("Start listening.")
     client.setBroadcast(true);
-	var setSound = rfidReader.updateReaderCommand("192.168.1.217", "255.255.255.0", "192.168.1.100", 0, "199-1-112-40", 1);
+	var setSound = rfidReader.setSoundCommand(0, rfidReader.soundType.shortBeepOnce);
 	client.send(setSound, 0, setSound.length, deviceBroadcastPort, broadcastAddress, failOnError);
+	var deviceData = data[1];
+	var updateReaderCommand = rfidReader.updateReaderCommand(deviceData.ip, deviceIpMask, deviceData.gateway, 0, deviceData.serial, 1);
+	client.send(updateReaderCommand, 0, updateReaderCommand.length, deviceBroadcastPort, broadcastAddress, function (err) {
+		console.log(arguments);
+		if (err) {
+			throw err;
+		}
+		
+		var setSound = rfidReader.setSoundCommand(0, rfidReader.soundType.shortBeepOnce);
+		client.send(setSound, 0, setSound.length, deviceBroadcastPort, broadcastAddress, failOnError);
+	});
 });
 
 client.bind({
