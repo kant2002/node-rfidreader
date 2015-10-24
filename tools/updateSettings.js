3c991eaf1a7e6ce1b595f941d165dbbb605a000f
Andrey Kurdyumov <kant2002@gmail.com>
2015-10-24 14:59:55 +0600

    Add more settings

--- tools/updateSettings.js
+++ tools/updateSettings.js
@@ -16,7 +16,9 @@ client.on("error", function (err) {
   client.close();
 });
 
-var deviceIpMask = "255.255.255.0";
+//var deviceIpMask = "255.255.255.0";
+//var deviceIpMask = "255.255.240.0";
+var deviceMask = "255.255.254.0";
 var data = [
 {
 	ip: "192.168.1.218",
@@ -27,6 +29,78 @@ var data = [
 	ip: "192.168.1.217",
 	gateway: "192.168.1.108",
 	serial: "199-1-112-40"
+},
+// 01
+{
+	ip: "10.240.66.10",
+	gateway: "10.240.66.1",
+	serial: "193-1-110-217"
+},
+// 02
+{
+	ip: "10.240.66.11",
+	gateway: "10.240.66.1",
+	serial: "141-1-33-145"
+},
+// 03
+{
+	ip: "10.240.66.12",
+	gateway: "10.240.66.1",
+	serial: "193-1-110-141"
+},
+// 04
+{
+	ip: "10.240.66.13",
+	gateway: "10.240.66.1",
+	serial: "144-1-31-91"
+},
+// 05
+{
+	ip: "10.240.66.14",
+	gateway: "10.240.66.1",
+	serial: "144-1-31-84"
+},
+// 06
+{
+	ip: "10.240.66.15",
+	gateway: "10.240.66.1",
+	serial: "144-1-31-119"
+},
+// 07
+{
+	ip: "10.240.66.16",
+	gateway: "10.240.66.1",
+	serial: "152-1-34-192"
+},
+// 08
+{
+	ip: "10.240.66.17",
+	gateway: "10.240.66.1",
+	serial: "193-1-110-184"
+},
+// 09
+{
+	ip: "10.240.66.18",
+	gateway: "10.240.66.1",
+	serial: "172-1-115-52"
+},
+// 10
+{
+	ip: "10.240.66.19",
+	gateway: "10.240.66.1",
+	serial: "146-1-34-58"
+},
+// 11
+{
+	ip: "10.240.66.20",
+	gateway: "10.240.66.1",
+	serial: "172-1-115-88"
+},
+// 12
+{
+	ip: "10.240.66.21",
+	gateway: "10.240.66.1",
+	serial: "141-1-33-202"
 }
 ];
 
@@ -37,7 +111,7 @@ client.on("listening", function () {
     client.setBroadcast(true);
 	var setSound = rfidReader.setSoundCommand(0, rfidReader.soundType.shortBeepOnce);
 	client.send(setSound, 0, setSound.length, deviceBroadcastPort, broadcastAddress, failOnError);
-	var deviceData = data[1];
+	var deviceData = data[13];
 	var updateReaderCommand = rfidReader.updateReaderCommand(deviceData.ip, deviceIpMask, deviceData.gateway, 0, deviceData.serial, 1);
 	client.send(updateReaderCommand, 0, updateReaderCommand.length, deviceBroadcastPort, broadcastAddress, function (err) {
 		console.log(arguments);
@@ -52,5 +126,5 @@ client.on("listening", function () {
 
 client.bind({
 	port: deviceBroadcastPort,
-	//address: "169.254.167.154"
+	address: "169.254.167.154"
 });
